8481282d00b6fbef82e95f293d0aa38e95e3f099
Patrick <patrick.hurley@codeimmersives.com>
2020-04-08 10:00:35 -0400

    finished

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const newPerson = function(firstName = 'Anonymous', lastName = 'Person', age, married = false){
+  
+  const newObj = {
+    firstName: firstName,
+    lastName: lastName,
+    age: age,
+    married: married,
+
+    goingOn: function(){
+      newAge = this.age
+    return newAge + 1
+    },
+
+    ageUp: function(){
+   this.age = this.age + 1
+    },
+    getFullName: function(){
+      return `${this.firstName} ${this.lastName}`
+    },
+    marry: function(person){
+      this.married = true
+      person.married = true
+      this.spouseName = person.getFullName()
+      person.spouseName = this.getFullName()
+    },
+
+    divorce: function(person){
+      this.married = false
+      delete this.spouseName
+      person.married = false
+      delete person.spouseName
+    }
+  };
+return newObj;
+};
+
+
 
 
 
